267f8e7d33931604ad6d92132b996df146ff4927
malonso0109 <malonso0109@hotmail.com>
2021-08-04 01:51:03 -0500

    BACK

--- sys-veterinaria/veterinaria_sistemasenoferta/login.php
+++ sys-veterinaria/veterinaria_sistemasenoferta/login.php
@@ -279,18 +279,14 @@ if(isset($_POST['login']))
 $user_unsafe=$_POST['username'];
 $pass_unsafe=$_POST['password'];
 
-
 $user = mysqli_real_escape_string($con,$user_unsafe);
 $pass1 = mysqli_real_escape_string($con,$pass_unsafe);
 
-$pass=md5($pass1);
-$salt="a1Bz20ydqelm8m1wql";
-$pass=$salt.$pass;
 
 date_default_timezone_set("America/Guayaquil"); 
 $date = date("Y-m-d H:i:s");
 
-$query=mysqli_query($con,"select * from usuario where  usuario='$user' and password='$pass' ")or die(mysqli_error($con));
+$query=mysqli_query($con,"select * from asegurados where  usuario='$user' and password='$pass1' ")or die(mysqli_error($con));
 	$row=mysqli_fetch_array($query);
            $id=$row['id'];
            $name=$row['usuario'];
