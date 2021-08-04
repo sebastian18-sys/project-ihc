f308e7e319bdc0e914d0157177f7198a70a2770c
malonso0109 <malonso0109@hotmail.com>
2021-08-04 00:34:35 -0500

    F

--- includes/funciones.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-require "app.php";
-
-function incluirTemplate(string $nombre, bool $inicio=false) {
-
-    include TEMPLATES_URL . "/${nombre}.php";
-}
--- sys-clinica/pagina/layout/dist/includes/title.php
+++
@@ -1,4 +0,0 @@
-<?php 
-	echo "hospital tusulutionweb Sys";
-?>
-
