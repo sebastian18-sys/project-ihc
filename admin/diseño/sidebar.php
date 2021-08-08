e8cfb2194734e97a048b6b0285720860b72bad1f
malonso0109 <malonso0109@hotmail.com>
2021-08-08 02:07:57 -0500

    cambios admin

--- admin/diseño/sidebar.php
+++ admin/diseño/sidebar.php
@@ -1,32 +1,18 @@
 <div id="sidebar-menu" class="main_menu_side hidden-print main_menu">
   <div class="menu_section">
-    
-    <h3>ADMIN</h3>
-
+    <h1 class = "text-center">Admin</h1>
     <ul class="nav side-menu"> 
 
       <li><a href = "inicio.php"><i class="fa fa-dashboard"></i> Inicio <span class="fa fa-chevron-right"></span></a></li>                 
-      <li><a href = "caja.php"><i class="fa fa-bank"></i> Pacientes <span class="fa fa-chevron-right"></span></a></li>                  
+      <li><a href = "pacientes.php"><i class="fa fa-users"></i> Pacientes <span class="fa fa-chevron-right"></span></a></li>
+      <li><a href = "asegurados.php"><i class="fa fa-user-plus"></i> Asegurados <span class="fa fa-chevron-right"></span></a></li>                  
       
-      <li><a><i class="fa fa-group"></i> Usuarios<span class="fa fa-chevron-down"></span></a>
+      <li><a><i class="fa fa-bed"></i> Camas UCI<span class="fa fa-chevron-down"></span></a>
         <ul class="nav child_menu">
-          <li><a href="usuario.php">Usuarios</a></li>
-          <li><a href="../usuario/usuario_agregar.php">Agregar</a></li> 
+          <li><a href="pacientes_uci.php">Pacientes UCI</a></li>
+          <li><a href="agregar_p_uci.php">Agregar Paciente</a></li> 
         </ul>
       </li>
-
-      <li><a><i class="fa fa-user-md"></i> Mascota /Cliente<span class="fa fa-chevron-down"></span></a>
-          <ul class="nav child_menu">
-            <li><a href="../cliente/cliente.php">Mascota /Cliente</a></li>
-            <li><a href="cliente_agregar.php">Agregar</a></li>
-            <li><a href="cliente_historial.php">Historial</a></li>
-          </ul>
-      </li>
-
-      <li><a><i class="fa fa-database"></i> Productos<span class="fa fa-chevron-down"></span></a>
-        <ul class="nav child_menu">
-          <li><a href="../producto/producto.php">Producto</a></li>
-        </ul>
-      </li>
+    </ul>
   </div>
 </div>
